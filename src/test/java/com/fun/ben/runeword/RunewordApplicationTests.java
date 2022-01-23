94a2e32ae6acc445d3fe2eabf5ef51143cc79bb7
hiltop2002 <30682366+hiltop2002@users.noreply.github.com>
2022-01-22 16:36:22 -0800

    Initial commit

---
+++ src/test/java/com/fun/ben/runeword/RunewordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fun.ben.runeword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunewordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
